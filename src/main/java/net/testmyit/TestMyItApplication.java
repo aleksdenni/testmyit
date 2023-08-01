72a17458f182962b53a0cdbefc3b1adcc3496d0c
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-08-02 02:14:28 +0300

    Initial commit

---
+++ src/main/java/net/testmyit/TestMyItApplication.java
@@ -0,0 +1,13 @@
+package net.testmyit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMyItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMyItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/testmyit/TestMyItApplicationTests.java
@@ -0,0 +1,13 @@
+package net.testmyit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMyItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
