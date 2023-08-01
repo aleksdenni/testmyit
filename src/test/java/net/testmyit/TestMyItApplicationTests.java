5c978074bdc99a190a9c36242ccb2e7de35c31a9
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-08-02 02:15:55 +0300

    init

--- src/test/java/net/testmyit/TestMyItApplicationTests.java
+++ src/test/java/net/testmyit/TestMyItApplicationTests.java
@@ -2,12 +2,16 @@ package net.testmyit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TestMyItApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
