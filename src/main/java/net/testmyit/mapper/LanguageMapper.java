6d678ababa5dfcbbffe839847d290d544172053b
iterepa <wizmond705@gmail.com>
2023-09-10 12:06:13 +0300

    create LanguageMapper

---
+++ src/main/java/net/testmyit/mapper/LanguageMapper.java
@@ -0,0 +1,34 @@
+package net.testmyit.mapper;
+
+
+import net.testmyit.dto.LanguageDto;
+import net.testmyit.dto.request.LanguageRequestDto;
+import net.testmyit.model.Language;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LanguageMapper {
+
+    public Language toLanguage(LanguageRequestDto languageRequestDto){
+        return Language.builder()
+                .language(languageRequestDto.getLanguage())
+                .image(languageRequestDto.getImage())
+                .build();
+    }
+
+    public LanguageDto toDto(Language language){
+        return LanguageDto.builder()
+                .id(language.getId())
+                .language(language.getLanguage())
+                .image(language.getImage())
+                .build();
+    }
+
+    public Language toLanguage(LanguageDto languageDto) {
+        return Language.builder()
+                .id(languageDto.getId())
+                .language(languageDto.getLanguage())
+                .image(languageDto.getImage())
+                .build();
+    }
+}
