80281fe8c0e32e430d96a8eea7184ed41cc44ec3
iterepa <wizmond705@gmail.com>
2023-09-10 11:35:59 +0300

    change id type to long

--- src/main/java/net/testmyit/model/Language.java
+++ src/main/java/net/testmyit/model/Language.java
@@ -17,7 +17,7 @@ public class Language {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;
 
     @Column(name = "language")
     private String language;
