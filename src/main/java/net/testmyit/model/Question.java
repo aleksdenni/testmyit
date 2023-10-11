6ff141345c6b47dc452d5639621bba3c7c588240
iterepa <wizmond705@gmail.com>
2023-10-11 11:38:53 +0300

    add Question model, need to be checked

---
+++ src/main/java/net/testmyit/model/Question.java
@@ -0,0 +1,30 @@
+package net.testmyit.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "question", nullable = false)
+    private String question;
+
+    @OneToMany
+    @Column(name = "option_id", nullable = false)
+    private List<Option> option_id;
+}
