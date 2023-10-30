4f1ee5a554b76c974ee15cca8d26f52dcb149ab5
frogiri <wizmond705@gmail.com>
2023-10-30 15:07:24 +0200

    Create mapper and controller

--- src/main/java/net/testmyit/controller/OptionController.java
+++ src/main/java/net/testmyit/controller/OptionController.java
@@ -1,4 +1,35 @@
 package net.testmyit.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.testmyit.dto.request.OptionRequestDto;
+import net.testmyit.dto.response.OptionResponseDto;
+import net.testmyit.service.OptionService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/options")
+@RequiredArgsConstructor
 public class OptionController {
+    private final OptionService optionService;
+
+    @PostMapping
+    public OptionResponseDto createOption(@RequestBody @Valid OptionRequestDto optionRequestDto) {
+        return optionService.createOption(optionRequestDto);
+    }
+
+    @GetMapping("/id")
+    public OptionResponseDto getOption(@PathVariable Long id) {
+        return optionService.getOption(id);
+    }
+
+    @PutMapping("/id")
+    public OptionResponseDto updateOption(@PathVariable Long id, @Valid @RequestBody OptionRequestDto optionRequestDto) {
+        return optionService.updateOption(id, optionRequestDto);
+    }
+
+    @DeleteMapping("/id")
+    public void deleteOption(@PathVariable Long id) {
+        optionService.deleteOption(id);
+    }
 }
