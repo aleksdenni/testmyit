57d415d91c6c6cd2050d8463c06584e67de63447
frogiri <wizmond705@gmail.com>
2023-11-02 13:38:03 +0200

    Implemented Dto questions

--- src/main/java/net/testmyit/dto/request/QuestionRequestDto.java
+++ src/main/java/net/testmyit/dto/request/QuestionRequestDto.java
@@ -1,4 +1,15 @@
 package net.testmyit.dto.request;
 
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import net.testmyit.model.Category;
+
+@Builder
+@Value
+@RequiredArgsConstructor
 public class QuestionRequestDto {
+    @NonNull String question;
+    @NonNull Category category_id;
 }
--- src/main/java/net/testmyit/dto/response/QuestionResponseDto.java
+++ src/main/java/net/testmyit/dto/response/QuestionResponseDto.java
@@ -1,4 +1,16 @@
 package net.testmyit.dto.response;
 
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import net.testmyit.model.Category;
+
+@Value
+@Builder
+@RequiredArgsConstructor
 public class QuestionResponseDto {
+    @NonNull Long id;
+    @NonNull String question;
+    @NonNull Category category_id;
 }
