9c01704820b0392527ab899e710fc790639a4c47
frogiri <wizmond705@gmail.com>
2023-11-02 13:38:18 +0200

    Implemented repository questions

--- src/main/java/net/testmyit/repository/QuestionRepository.java
+++ src/main/java/net/testmyit/repository/QuestionRepository.java
@@ -1,7 +1,7 @@
 package net.testmyit.repository;
 
 import net.testmyit.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+public interface QuestionRepository extends CrudRepository<Question, Long> {
 }
