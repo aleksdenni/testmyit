0efc8d4783bad9eb00b2b6b9af18171a7bcbf828
frogiri <wizmond705@gmail.com>
2023-11-02 13:38:32 +0200

    Implement mapper for questions

--- src/main/java/net/testmyit/mapper/QuestionMapper.java
+++ src/main/java/net/testmyit/mapper/QuestionMapper.java
@@ -1,4 +1,33 @@
 package net.testmyit.mapper;
 
+import net.testmyit.dto.request.QuestionRequestDto;
+import net.testmyit.dto.response.QuestionResponseDto;
+import net.testmyit.model.Question;
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuestionMapper {
+
+    public Question toEntity(QuestionRequestDto questionRequestDto) {
+        return Question.builder()
+                .question(questionRequestDto.getQuestion())
+                .category_id(questionRequestDto.getCategory_id())
+                .build();
+    }
+
+    public Question toEntity(QuestionResponseDto questionResponseDto) {
+        return Question.builder()
+                .id(questionResponseDto.getId())
+                .question(questionResponseDto.getQuestion())
+                .category_id(questionResponseDto.getCategory_id())
+                .build();
+    }
+
+    public QuestionResponseDto toDto(Question question) {
+        return QuestionResponseDto.builder()
+                .id(question.getId())
+                .question(question.getQuestion())
+                .category_id(question.getCategory_id())
+                .build();
+    }
 }
