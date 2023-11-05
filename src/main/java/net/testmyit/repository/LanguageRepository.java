3c84975042aa241ad04c2550f99f31dbcf0b384d
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-11-05 15:56:32 +0200

    hide extra endpoints

--- src/main/java/net/testmyit/repository/LanguageRepository.java
+++ src/main/java/net/testmyit/repository/LanguageRepository.java
@@ -1,9 +1,10 @@
 package net.testmyit.repository;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import net.testmyit.model.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+@Hidden
 @Repository
 public interface LanguageRepository extends JpaRepository<Language, Long> {
 }
--- src/main/java/net/testmyit/repository/UserRepository.java
+++ src/main/java/net/testmyit/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package net.testmyit.repository;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import net.testmyit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+@Hidden
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
