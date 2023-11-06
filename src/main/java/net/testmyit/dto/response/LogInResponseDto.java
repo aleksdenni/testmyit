75aef403c69ccac3715e6ec8dd4f3282c594e2c9
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-11-06 10:28:16 +0200

    change @Data to @Value (lombok)

--- src/main/java/net/testmyit/dto/response/LogInResponseDto.java
+++ src/main/java/net/testmyit/dto/response/LogInResponseDto.java
@@ -1,21 +1,18 @@
 package net.testmyit.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Value
 @Builder
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
 public class LogInResponseDto {
 
     @JsonProperty("access_token")
-    private String accessToken;
+    String accessToken;
 
     @JsonProperty("refresh_token")
-    private String refreshToken;
+    String refreshToken;
 
 }
