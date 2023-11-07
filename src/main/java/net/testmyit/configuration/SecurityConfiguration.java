515f0714f0d3e2bb0b910bcf96fc1bc38df1d3c5
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-11-07 23:09:17 +0200

    connect OAuth and jwt

--- src/main/java/net/testmyit/configuration/SecurityConfiguration.java
+++ src/main/java/net/testmyit/configuration/SecurityConfiguration.java
@@ -1,22 +1,31 @@
 package net.testmyit.configuration;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
+    @Value("${keycloak.jwkSetUri}")
+    private String jwkSetUri;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
-        return http.build();
+        http.authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.csrf(AbstractHttpConfigurer::disable).build();
     }
 
     @Bean
@@ -25,7 +34,11 @@ public class SecurityConfiguration {
                 .requestMatchers(HttpMethod.POST, "*/api/v1/users", "/api/v1/auth/login")
                 .requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/api/v1/auth/logout"
                         , "/actuator/**", "/swagger-ui/**");
+    }
 
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 
 }
