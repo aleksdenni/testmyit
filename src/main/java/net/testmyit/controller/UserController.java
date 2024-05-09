ff5b8ee76a1c4afc721d309742419e8929a186f3
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2024-05-09 18:08:17 +0300

    add ResponseEntity

--- src/main/java/net/testmyit/controller/UserController.java
+++ src/main/java/net/testmyit/controller/UserController.java
@@ -1,11 +1,15 @@
 package net.testmyit.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import net.testmyit.dto.request.UserRequestDto;
 import net.testmyit.dto.response.UserResponseDto;
 import net.testmyit.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -14,22 +18,32 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserResponseDto createUser(@RequestBody @Valid UserRequestDto userRequest) {
-        return userService.createUser(userRequest);
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody @Valid UserRequestDto userRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.createUser(userRequest));
     }
 
     @GetMapping("/{id}")
-    public UserResponseDto getUser(@PathVariable Long id) {
-        return userService.getUser(id);
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.getUser(id));
     }
 
+    @Hidden
     @PutMapping("/{id}")
-    public UserResponseDto updateUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
-        return userService.updateUser(id, userRequestDto);
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.updateUser(id, userRequestDto));
     }
 
+    @Hidden
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
